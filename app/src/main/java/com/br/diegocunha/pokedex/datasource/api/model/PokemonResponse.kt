4ec45b5c93a94c6b5a5b2f38a73df8ba342e004a
Diego Cunha <domdiegobc@gmail.com>
2022-09-03 09:41:07 -0300

    feature :: Added room

--- app/src/main/java/com/br/diegocunha/pokedex/datasource/api/model/PokemonResponse.kt
+++ app/src/main/java/com/br/diegocunha/pokedex/datasource/api/model/PokemonResponse.kt
@@ -1,7 +1,9 @@
 package com.br.diegocunha.pokedex.datasource.api.model
 
 import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
+import com.br.diegocunha.pokedex.datasource.core.SlotType
+import com.br.diegocunha.pokedex.datasource.core.Sprites
+import com.br.diegocunha.pokedex.datasource.core.Stats
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -36,98 +38,4 @@ data class SinglePokemonResult(
     val height: Int,
     val weight: Int,
     val types: List<SlotType>
-) : Parcelable
-
-@Parcelize
-data class SlotType(
-    val type: Type
-) : Parcelable
-
-@Parcelize
-data class Type(
-    val name: PokemonType,
-    val url: String
-) : Parcelable
-
-enum class PokemonType {
-    @SerializedName("normal")
-    NORMAL,
-
-    @SerializedName("fighting")
-    FIGHTING,
-
-    @SerializedName("flying")
-    FLYING,
-
-    @SerializedName("poison")
-    POISON,
-
-    @SerializedName("ground")
-    GROUND,
-
-    @SerializedName("rock")
-    ROCK,
-
-    @SerializedName("bug")
-    BUG,
-
-    @SerializedName("ghost")
-    GHOST,
-
-    @SerializedName("steel")
-    STEEL,
-
-    @SerializedName("fire")
-    FIRE,
-
-    @SerializedName("water")
-    WATER,
-
-    @SerializedName("grass")
-    GRASS,
-
-    @SerializedName("electric")
-    ELECTRIC,
-
-    @SerializedName("psychic")
-    PSYCHIC,
-
-    @SerializedName("ice")
-    ICE,
-
-    @SerializedName("dragon")
-    DRAGON,
-
-    @SerializedName("dark")
-    DARK,
-
-    @SerializedName("fairy")
-    FAIRY,
-
-    @SerializedName("unknown")
-    UNKNOWN,
-
-    @SerializedName("shadow")
-    SHADOW
-}
-
-@Parcelize
-data class Sprites(
-    val back_default: String? = null,
-    val back_shiny: String? = null,
-    val front_default: String? = null,
-    val front_shiny: String? = null
-) : Parcelable
-
-@Parcelize
-data class Stats(
-    val base_stat: Int,
-    val effort: Int,
-    val stat: Stat
-) : Parcelable
-
-@Parcelize
-data class Stat(
-    val name: String,
-    val url: String
 ) : Parcelable
---
+++ app/src/main/java/com/br/diegocunha/pokedex/datasource/core/PokemonCore.kt
@@ -0,0 +1,99 @@
+package com.br.diegocunha.pokedex.datasource.core
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class SlotType(
+    val type: Type
+) : Parcelable
+
+@Parcelize
+data class Type(
+    val name: PokemonType,
+    val url: String
+) : Parcelable
+
+enum class PokemonType {
+    @SerializedName("normal")
+    NORMAL,
+
+    @SerializedName("fighting")
+    FIGHTING,
+
+    @SerializedName("flying")
+    FLYING,
+
+    @SerializedName("poison")
+    POISON,
+
+    @SerializedName("ground")
+    GROUND,
+
+    @SerializedName("rock")
+    ROCK,
+
+    @SerializedName("bug")
+    BUG,
+
+    @SerializedName("ghost")
+    GHOST,
+
+    @SerializedName("steel")
+    STEEL,
+
+    @SerializedName("fire")
+    FIRE,
+
+    @SerializedName("water")
+    WATER,
+
+    @SerializedName("grass")
+    GRASS,
+
+    @SerializedName("electric")
+    ELECTRIC,
+
+    @SerializedName("psychic")
+    PSYCHIC,
+
+    @SerializedName("ice")
+    ICE,
+
+    @SerializedName("dragon")
+    DRAGON,
+
+    @SerializedName("dark")
+    DARK,
+
+    @SerializedName("fairy")
+    FAIRY,
+
+    @SerializedName("unknown")
+    UNKNOWN,
+
+    @SerializedName("shadow")
+    SHADOW
+}
+
+@Parcelize
+data class Sprites(
+    val back_default: String? = null,
+    val back_shiny: String? = null,
+    val front_default: String? = null,
+    val front_shiny: String? = null
+) : Parcelable
+
+@Parcelize
+data class Stats(
+    val base_stat: Int,
+    val effort: Int,
+    val stat: Stat
+) : Parcelable
+
+@Parcelize
+data class Stat(
+    val name: String,
+    val url: String
+) : Parcelable
